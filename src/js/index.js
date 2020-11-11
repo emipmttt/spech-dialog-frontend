a04375d69ef19bef7efa5e00bd0fb097c9190ef6
Emiliano Pamont <emipmttt@gmail.com>
2020-11-11 12:17:05 -0600

    .

---
+++ src/js/index.js
@@ -0,0 +1,116 @@
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; //stream from getUserMedia()
+var rec; //Recorder.js object
+var input; //MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+
+function startRecording() {
+    console.log("recordButton clicked");
+
+    /*
+    	Simple constraints object, for more advanced audio features see
+    	https://addpipe.com/blog/audio-constraints-getusermedia/
+    */
+
+    var constraints = {
+        audio: true,
+        video: false
+    }
+
+    /*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
+
+    recordButton.style.display = "none";
+    stopButton.style.display = "initial";
+
+    /*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+        /*
+        	create an audio context after getUserMedia is called
+        	sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+        	the sampleRate defaults to the one set in your OS for your playback device
+        */
+        audioContext = new AudioContext();
+
+        //update the format 
+        document.getElementById("formats").innerHTML = "Format: 1 channel pcm @ " + audioContext.sampleRate / 1000 + "kHz"
+
+        /*  assign to gumStream for later use  */
+        gumStream = stream;
+
+        /* use the stream */
+        input = audioContext.createMediaStreamSource(stream);
+
+        /* 
+        	Create the Recorder object and configure to record mono sound (1 channel)
+        	Recording 2 channels  will double the file size
+        */
+        rec = new Recorder(input, {
+            numChannels: 1
+        })
+
+        //start the recording process
+        rec.record()
+
+        console.log("Recording started");
+
+    }).catch(function (err) {
+        //enable the record button if getUserMedia() fails
+        recordButton.style.display = "initial";
+        stopButton.style.display = "none";
+    });
+}
+
+function stopRecording() {
+    console.log("stopButton clicked");
+
+    //disable the stop button, enable the record too allow for new recordings
+    recordButton.style.display = "initial";
+    stopButton.style.display = "none";
+
+    //tell the recorder to stop the recording
+    rec.stop();
+
+    //stop microphone access
+    gumStream.getAudioTracks()[0].stop();
+
+    //create the wav blob and pass it on to createDownloadLink
+    rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('voice', blob);
+
+    axios.post("http://localhost:3000/voice", bodyFormData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(response => {
+        const audioPath = 'http:/localhost:3000/' + response.data.data.replace(/storage/g, "")
+        console.log(audioPath);
+        var audio = new Audio(audioPath);
+        audio.play();
+    })
+}
